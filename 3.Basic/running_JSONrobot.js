0a17b3d3a268fed0093f682cbef81c87adbbb5c5
Prateek Narang <prateekrishu@yahoo.com>
2015-09-07 14:25:25 +0530

    Added an animated robot that moves between boundaries

---
+++ 3.Basic/running_JSONrobot.js
@@ -0,0 +1,39 @@
+var game = new Phaser.Game(800,600,Phaser.CANVAS,'canvas', {preload:preload,create:create,update:update,render:render});
+var bot;
+
+function preload(){
+	game.load.atlasJSONHash('bot','../assets/running_bot.png','../assets/running_bot.json');
+
+}
+
+
+function create(){
+	bot = game.add.sprite(450,450,'bot');
+
+	// Add one or more animations and play it . You can also specify the frames to play.
+	bot.animations.add('run');
+	bot.animations.play('run',15,true);	
+
+	//game.physics.startSystem(Phaser.Physics.ARCADE);
+	
+	//NoTE : THis way should be preferred to Enable Physics on an object.
+	// The older way  
+	game.physics.arcade.enable(bot);
+	bot.body.velocity.x = -100;
+	console.log("")
+
+}
+function update(){
+
+	if(bot.body.x <=0){
+		bot.body.velocity.x = +100;
+	}
+	else if(bot.body.x>=800){
+		bot.body.velocity.x = -100;
+	}	
+
+}
+
+function render(){
+	game.debug.inputInfo(32,32);
+}
